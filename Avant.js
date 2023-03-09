7d3be64da00598715a21fa93d591dc71e96ae1fa
TEENDERU VAISHNAVI <102427538+TEENDERUVAISHNAVI@users.noreply.github.com>
2023-03-09 22:49:23 +0530

    Create Avant.js

---
+++ Avant.js
@@ -0,0 +1,20 @@
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+  };
+};
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
